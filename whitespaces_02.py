61dcdf5bc6b3795d4729dfdfca376eba58ebd998
Aydin_tech <sajalnazir6@gmail.com>
2025-06-09 05:54:05 -0700

    Add files via upload
    Here is how to deal with white spaces problem in python- In this program I applied rules of python language to solve this problem effectively

---
+++ whitespaces_02.py
@@ -0,0 +1,18 @@
+# we are gonna understand different tabs for different purposes-
+
+# use '\t' for tab-
+name = "code with Aydin"
+print(name)
+name = "\t code with Aydin"
+print(name)
+
+# use '\n' for new line-
+a = "apple,aeroplane,"
+print(a)
+b = "objects with b: \t\nball\t\nballoon"
+print(b)
+
+# Removing extra spaces from code or string- / more examples can be written in TERMINAL
+c= "'blue cat'"
+d = "is my favourite"
+print(c+d.rstrip())
