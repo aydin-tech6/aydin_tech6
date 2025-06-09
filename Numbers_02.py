06eef4c01b9fefe70c184fbe185d4e3cae35c408
Aydin_tech <sajalnazir6@gmail.com>
2025-06-09 05:48:30 -0700

    Add files via upload
    In this file I coded numbers in python applying 4 mathematical calculations and also showed my mistakes from which you can learn- Everything taught in Python Crash Course third edition about numbers of different kinds is programmed in this file-

---
+++ Numbers_02.py
@@ -0,0 +1,57 @@
+ # the case of INTEGERS is one of most simplest in python-
+
+# adding integers
+a = 2+3
+print(a)
+
+# subtracting integers-
+b = 56-89
+print(b)
+
+# multiplying integers-
+multiply = 55*5
+print(multiply)
+
+# dividing integers-
+d = 55/5
+print(d)
+
+# in python exponent is represented by '**' instead of '^'
+exponenet = 3**2
+print(exponenet)
+
+# you can use python for solving large operatipons like-
+operation=3*5+7
+print(operation)
+
+high_operation = 234+456*678/9876
+print(high_operation)
+
+
+# WORKING WITH FLOATS- FLOATS MEANS ANY NUMBER WITH DECIMALS-
+
+# python work with floats the same way it does with integers-
+print(7.6+1.23)
+
+print(67.0-98.87)
+
+print(99.0*45.76)
+
+print(65.0/5.0)
+
+# using uderscores in numbers is for ease and python ignores it while showing output-
+my_salary = 10_000_0_000
+print(my_salary)
+
+# assigning multiple values or numbers to variable-
+a,b,c = 56,12,89
+print(a,b,c)
+
+# now lets chexk if same works for strings- i do not think it will work-
+
+
+# yeah i was right- i did not work-you can ry yourself in the above empty space-
+
+# use all capital letters while naming variable to treat it as constant-
+MY_NAME = "Noor Nazir"
+print(MY_NAME)
